75ee46c2977cb5260c68e42f40236a4fa80c82df
jhhere <joyicecloud@gmail.com>
2021-09-04 13:49:37 -0700

    Add custom post types to the event with date picker

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,10 @@
 <?php
 function university_post_types() {
     register_post_type('event', array(
+      'show_in_rest' => true,
+      'supports' => array('title', 'editor', 'excerpt'),
+      'rewrite' => array('slug' => 'events'),
+      'has_archive' => true,
       'public' => true,
       'labels' => array(
         'name' => 'Events',
@@ -12,7 +16,7 @@ function university_post_types() {
       'menu_icon' => 'dashicons-calendar'
     ));
   }
-  
+   
   add_action("init", "university_post_types");
 
   ?>
